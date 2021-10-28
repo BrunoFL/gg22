281af77117d14aa7d32dcbd21ac2f04be106bfc3
Bruno FL <follet-bruno@hotmail.fr>
2021-10-28 11:28:44 +0200

    pwa

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
+const { GenerateSW } = require("workbox-webpack-plugin");
+
 module.exports = {
   pwa: {
     // configure the workbox plugin
     workboxPluginMode: 'GenerateSW'
+  },
+  configureWebpack: {
+    plugins: [new GenerateSW()]
   }
 }
