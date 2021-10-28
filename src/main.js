28270d0724d775961696a43adf55a81bdba0de40
Bruno FL <follet-bruno@hotmail.fr>
2021-10-28 11:49:23 +0200

    pwa

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './registerServiceWorker'
+
 import VueSocketIOExt from 'vue-socket.io-extended';
 import { io } from 'socket.io-client';
 
