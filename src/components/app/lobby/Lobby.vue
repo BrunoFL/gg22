7d2d11cfd0a775df0606c8b5c4bafbde3fafc814
Nathan Piranda <nathan.piranda@symphonyretailai.com>
2022-01-28 22:04:39 +0100

    added Lobby component

---
+++ src/components/app/lobby/Lobby.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="mainLobby">
+    <h1>NOM DU JEU</h1>
+
+    <!-- SCREEN CHOICE -->
+    <div v-if="!isClicked"  id="firstScreen">
+      <b-button v-on:click="createLobby()">Create lobby</b-button>
+      <b-button v-on:click="joinLobby()">Join lobby</b-button>
+    </div>
+
+    <!-- BLOC CREATE LOBBY-->
+    <div v-if="clickCreateLobby" id="createLobby">
+      <b-input-group size="lg" prepend="Nom du lobby">
+        <b-form-input></b-form-input>
+        <b-input-group-append>
+          <b-button variant="outline-success">Créer la partie</b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </div>
+
+    <!-- BLOC JOIN LOBBY-->
+    <div v-if="clickJoinLobby" id="joinLobby">
+      <div v-for="lobby of lobbies" :key="lobby.name">
+        <p>{{lobby.name}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Lobby",
+  data(){
+    return {
+      isClicked: false,
+      clickJoinLobby: false,
+      clickCreateLobby: false,
+      lobbies: [{name:'komtuve'},{name: 'komtulsan'}]
+    }
+  },
+  methods: {
+    createLobby() {
+      //TODO : CREATE LOBBY
+      this.$socket.client.emit('createLobby')
+      this.isClicked = true
+      this.clickCreateLobby = true
+      this.$emit('swapScreen','create')
+    },
+    joinLobby() {
+      //TODO : JOIN LOBBY
+      this.$socket.client.emit('joinLobby')
+      this.isClicked = true
+      this.clickJoinLobby = true
+      this.$emit('swapScreen','join')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
