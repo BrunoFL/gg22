3b7fd86c662c1529e794160b17150b894e3ff6c6
Bruno FL <follet-bruno@hotmail.fr>
2022-01-29 11:13:32 +0100

    fix uuid

--- server/utils/UUIDGenerator.mjs
+++ server/utils/UUIDGenerator.mjs
@@ -1,14 +1,15 @@
 export class UUIDGenerator {
-
+    /**
+     * @return {string}
+     */
     static uuid() {
-        function createGUID() {
-            function random() {
-              return Math.floor((1 + Math.random()) * 0x10000)
+        function random() {
+            return Math.floor((1 + Math.random()) * 0x10000)
                 .toString(16)
-                .substring(1);
-            }
-            return random() + random() + '-' + random() + '-' + random() + '-' +
-              random() + '-' + random() + random() + random();
-          }
+                .substring(1)
+        }
+
+        return random() + random() + '-' + random() + '-' + random() + '-' +
+            random() + '-' + random() + random() + random()
     }
 }
