55ebe49ec88b7caf9a41035daee1aff3607e53a4
thibaudjeannin <thibaud.jeannin@gmail.com>
2022-01-29 13:59:42 +0100

    Rename PointScoringSystem

---
+++ server/entities/PointScoringSystem.mjs
@@ -0,0 +1,16 @@
+export class PointScoringSystem {
+
+    static pointScoringSystem = [10, 6, 4, 3, 2, 1]
+
+    /**
+     * @param {number} position 
+     * @param {boolean} finished 
+     * @returns 
+     */
+    static pointsFor(position, finished) {
+        if (position > PointScoringSystem.pointScoringSystem.length || !finished) {
+            return 0
+        }
+        return this.pointScoringSystem[position]
+    }
+}
