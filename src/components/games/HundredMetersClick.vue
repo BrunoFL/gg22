39903c4809e66f6d1271c576230b68ba630198d7
Nathan Piranda <nathan.piranda@symphonyretailai.com>
2022-01-29 17:29:35 +0100

    COURSE QUI MARCHE

--- src/components/games/HundredMetersClick.vue
+++ src/components/games/HundredMetersClick.vue
@@ -3,7 +3,10 @@
   <div id="rules">
     {{rulesRun}}
   </div>
-  <div v-for="player of run" :key="player.id">
+  <div class="group row align-items-start" v-for="player of run" :key="player.id">
+    <div class="row">
+      <span class="col-1" v-if="isMyId(player.player.socketId)">YOU</span>
+    </div>
     <b-progress :value="player.meter" :max="max" show-progress animated></b-progress>
   </div>
   <b-button type="button" class="btn btn-outline-primary" v-on:click="increment">
@@ -29,12 +32,16 @@ export default {
     increment(){
       this.$socket.client.emit("touch")
     },
-    valueBar(value){
-      return "width: " + value + "%;";
+    isMyId(id){
+      return id === this.$socket.client.id;
     }
+
   },
   sockets: {
     updateRun(data){
+      data.sort((a, b) => {
+        return b.meter - a.meter;
+      });
       this.run = data;
     },
     rules(rules){
