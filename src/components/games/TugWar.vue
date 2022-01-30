15a1697ef36ad022c79183cd1a8c77096191af79
Chrisdu25 <christopher.petitjean@gmail.com>
2022-01-30 10:18:09 +0100

    BOUTON D'CES MORTS

--- src/components/games/TugWar.vue
+++ src/components/games/TugWar.vue
@@ -2,14 +2,13 @@
   <div>
     <div id="race" v-if="!isRankingOpen" class="row justify-content-center gap-3">
       <p class="m-3" size="lg" style="background-color: white; font-size: 200%">{{ rules }}</p>
-      <b-button type="button" variant="warning" class="mx-auto" style="width: 33%;" v-on:click="increment">TIREZ !
-      </b-button>
     </div>
     <div class="m-5" style="font-size: 150%">
         <div style="float: left;">A</div>
         <span>{{ center }}</span>
         <div style="float: right;">B</div>
     </div>
+    <b-button type="button" variant="warning" class="mx-auto" style="width: 33%;" v-on:click="increment">TIREZ !</b-button>
     <GameRanking v-if="isRankingOpen" :rankingList="rankingList" @swapScreen="event => swapScreen(event)"></GameRanking>
   </div>
 </template>
